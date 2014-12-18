df69111ec8db76cc624a7eef2c336af95b369629
ran bernstein <bernstein.ran@gmail.com>
2014-12-18 08:34:12 +0200

    After recording Tara

--- Laban/LabanLib/domainAdaptation.py
+++ Laban/LabanLib/domainAdaptation.py
@@ -1,69 +1,86 @@
 import LabanUtils.combinationsParser as cp
 import LabanLib.LabanUtils.util as labanUtil
-from sklearn.linear_model import LinearRegression, LogisticRegression
 import numpy as np
 from sklearn.linear_model import ElasticNetCV
 import matplotlib.pyplot as plt
 from sklearn import metrics, svm
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.feature_selection import f_classif, SelectKBest
 from sklearn.multiclass import OneVsRestClassifier
 import matplotlib
 
-#X = np.array([[1, 2, 3, 4, 5], [5, 4, 3, 2, 1]])
-#y = np.array([0,  1])
 CMAs = ['Rachelle', 'Karen']
 trainSource = CMAs[0]
 testSource = CMAs[1]
 c=0.00001
-#clf = LogisticRegression(C=c,penalty='l1')
-clf = ElasticNetCV()
-X, Y, trainDs = labanUtil.getXYforMultiSet(trainSource)
-X_test_raw, Y_test, testDs = labanUtil.getXYforMultiSet(testSource)
-X_all = np.concatenate((X, X_test_raw))
  
-tags = [0]*len(X)+[1]*len(X_test_raw)
-clf.fit(X_all, tags)
-name = str(clf).split()[0].split('(')[0]
 
-sampleWeights = clf.predict(X)#sample_weight=sampleWeights
-CMAs = ['Rachelle', 'Karen']
-trainSource = CMAs[0]
-testSource = CMAs[1]
 withPCA=False
 fs=False
 c=80
-selectedFeaturesNum = 25
+selectedFeaturesNum = 70
 trndata, featuresNames = labanUtil.getPybrainDataSet(trainSource)  
 tstdata, featuresNames = labanUtil.getPybrainDataSet(testSource)
 X, Y = labanUtil.fromDStoXY(trndata)
 X_test, Y_test = labanUtil.fromDStoXY(tstdata)
-chooser=f_classif
-selector = SelectKBest(chooser, selectedFeaturesNum)
+X_all = np.concatenate((X, X_test))
+tags = [0]*len(X)+[1]*len(X_test)
+adaptor = ElasticNetCV()
+adaptorName = str(adaptor).split()[0].split('(')[0]
+filter=f_classif
+selector = SelectKBest(filter, selectedFeaturesNum)
+"""
 clf = svm.LinearSVC(C=c,  loss='LR',# kernel='linear',
                     penalty='L1', dual=False, 
                     class_weight='auto')
+"""
+clf = AdaBoostClassifier()
+clf_weights = AdaBoostClassifier()
+#c = svm.SVC(pena)
+#clf_weights = svm.SVC()
+#clf = svm.SVC()
 f1s=[]
 ps =[]
 rs=[]
+weightF1s=[]
 for y, y_test in zip(np.transpose(Y), np.transpose(Y_test)):
     selector.fit(X, y)
-    X = selector.transform(X)
-    clf.fit(X, y, sample_weight=sampleWeights)
-    pred = clf.predict(X_test)
-    f1s.append(metrics.f1_score(y, pred))
-    ps.append(metrics.precision_score(y, pred))
-    rs.append(metrics.recall_score(y, pred))
+    
+    X_all_filtered = selector.transform(X_all)
+    adaptor.fit(X_all_filtered, tags)
+    X_filtered = selector.transform(X)
+    X_test_filtered = selector.transform(X_test)
+    #print 'adaptor.predict(X_filtered)'
+    #print adaptor.predict(X_filtered)
+    #print 'adaptor.predict(X_test_filtered)'
+    #print adaptor.predict(X_test_filtered)
+    lowestSampleValue = 0.5
+    sampleWeights = np.array(adaptor.predict(X_filtered))
+    sampleWeights -= np.min(sampleWeights)
+    sampleWeights += lowestSampleValue
+    clf_weights.fit(X_filtered, y, sample_weight=sampleWeights)
+    weightPred = clf_weights.predict(X_test_filtered)
+    weightF1s.append(metrics.f1_score(y_test, weightPred))
+    #ps.append(metrics.precision_score(y, pred))
+    #rs.append(metrics.recall_score(y, pred))
+    
+    clf.fit(X_filtered, y)
+    pred = clf.predict(X_test_filtered)
+    f1s.append(metrics.f1_score(y_test, pred))
+    #ps.append(metrics.precision_score(y, pred))
+    #rs.append(metrics.recall_score(y, pred))
 
 
 m = np.mean(f1s)
 fig, ax = plt.subplots()
 qualities, combinations = cp.getCombinations()
 ind = np.arange(len(qualities))
-width = 0.25   
+width = 0.3   
 f1Rects = ax.bar(ind, f1s, width, color='g', label='F1: '+str(round(np.mean(f1s),3)) )
-pRecrs = ax.bar(ind+width, ps, width, color='b', label='Precision: '+str(round(np.mean(ps),3)))
-rRects = ax.bar(ind-width, rs, width, color='r', label='Recall: '+str(round(np.mean(rs),3)))
+weightF1Rects = ax.bar(ind+width, weightF1s, width, color='b', label='WeightF1: '+str(round(np.mean(weightF1s),3)) )
+#pRecrs = ax.bar(ind+width, ps, width, color='b', label='Precision: '+str(round(np.mean(ps),3)))
+#rRects = ax.bar(ind-width, rs, width, color='r', label='Recall: '+str(round(np.mean(rs),3)))
 ax.set_xticks(ind)
 #ax.set_xticklabels(ind)
 xtickNames = plt.setp(ax, xticklabels=qualities)
@@ -85,10 +102,9 @@ matplotlib.rc('font', **font)
 plt.title('CLF: '+name \
          + ', Train set: CMA #1'+' size-'+str(trainSize) \
          + ', Test set: CMA #2'+' size-'+str(testSize) \
-         + '\n Features num: '+str(vecLen) \
-         + ', Featue selection (FS) method: '+chooser.__name__ \
-         + ', Features num after FS: '+str(selectedFeaturesNum) \
-         +'\n with C: ' +str(c))
+         + '\n lowestSampleValue '+str(lowestSampleValue) \
+         + ', Featue selection (FS) method: '+filter.__name__ \
+         + ', Features num after FS: '+str(selectedFeaturesNum))
 plt.xlabel('Quality')
 plt.ylabel('F1 score')
 plt.show()
--- Laban/LabanLib/multiTask.py
+++ Laban/LabanLib/multiTask.py
@@ -10,19 +10,28 @@ from sklearn import cross_validation
 import copy
 import LabanLib.LabanUtils.combinationsParser as cp
 from sklearn.feature_selection import f_classif, SelectKBest, f_regression,RFECV
-from sklearn.linear_model import MultiTaskLassoCV, MultiTaskElasticNetCV
+from sklearn.linear_model import MultiTaskLassoCV, \
+    MultiTaskElasticNetCV, MultiTaskElasticNet, MultiTaskLasso
 
 CMAs = ['Rachelle', 'Milca', 'Sharon', 'Karen']
 ds, featuresNames = labanUtil.accumulateCMA(CMAs) 
 splitProportion=0.1
 #for e in params:
+testNum =10
+params = range(500,800,50)
+#params = np.linspace(0,1,10)
+#params = np.logspace(-11, -6, 20)
 precisions = []
 recalls = []
 testFs = []
 trainFs = []
-testNum =9
-params = range(testNum)
-for _ in params:
+#for selectedFeaureNum in params:
+ps = []
+rs = []
+teFs = []
+trFs = []
+selectedFeaureNum=500
+for test in range(testNum):
     tstdata, trndata = ds.splitWithProportion( splitProportion )
     #trndata = ds
     X_all, Y_all = labanUtil.getXYfromPybrainDS(ds)
@@ -37,7 +46,7 @@ for _ in params:
     #cvs = []
     #params = range(225,265)#np.linspace(0.1, 1, 10)
     #params = np.logspace(-12,-9, 15)
-    selectedFeaureNum = 125
+    #selectedFeaureNum =int(e)
     accum = np.zeros((X.shape[1],))
     for y in np.transpose(Y):
         selector = SelectKBest(f_classif, selectedFeaureNum)
@@ -48,9 +57,8 @@ for _ in params:
         return X[:, selectedIndices]     
     X_filtered, X_test_filtered, X_all_filtered =  \
     transform(X), transform(X_test), transform(X_all)
-    #print e
-    clf = MultiTaskElasticNetCV(eps=1e-11)
-    #clf = MultiTaskLassoCV(eps=1e-11)
+    clf = MultiTaskElasticNetCV(normalize=True)
+    #clf = MultiTaskLasso(normalize=True)
     clf.fit(X_filtered, Y)
     predTrain = np.array(clf.predict(X_filtered))
     splits = []
@@ -61,13 +69,27 @@ for _ in params:
     for col in range(pred.shape[1]):
         pred[:, col] = [1 if e>=splits[col] else 0 for e in pred[:, col]]
         predTrain[:, col] = [1 if e>=splits[col] else 0 for e in predTrain[:, col]]
-    precisions.append(metrics.precision_score(Y_test, pred))
-    recalls.append(metrics.recall_score(Y_test, pred))
-    testFs.append(metrics.f1_score(Y_test, pred))
-    trainFs.append(metrics.f1_score(Y, predTrain))
-    
-    #cvs.append(np.mean(cross_validation.cross_val_score(clf, X, y=y,
-    #       scoring=scorer, cv=2, verbose=True)))
+    ps.append(metrics.precision_score(Y_test, pred))
+    rs.append(metrics.recall_score(Y_test, pred))
+    teF  = metrics.f1_score(Y_test, pred)
+    teFs.append(teF)
+    trFs.append(metrics.f1_score(Y, predTrain))
+    print 'test#: ', test
+    p = np.mean(ps)
+    r = np.mean(rs)
+    f= 2*p*r/(p+r)
+    print  p, r, f
+#precisions.append(np.mean(ps))
+#recalls.append(np.mean(rs))
+p = np.mean(ps)
+r = np.mean(rs)
+f= 2*p*r/(p+r)
+print 'selectedFeaureNum: ',selectedFeaureNum
+print  p, r, f, np.mean(teFs)
+testFs.append(f)
+trainFs.append(np.mean(trFs))
+#cvs.append(np.mean(cross_validation.cross_val_score(clf, X, y=y,
+#       scoring=scorer, cv=2, verbose=True)))
 #print params
 print testNum
 print precisions
@@ -81,11 +103,16 @@ print trainFs
 name = str(clf).split()[0].split('(')[0]
 des = name+' selectedFeaureNum = '+str(selectedFeaureNum)
 des+=' testNum '+str(testNum)
+#des+=' e ' + str(e)
 print des
-plt.semilogx(params, trainFs, label='TrainEs F1, max: '+str(max(trainFs)))
-plt.semilogx(params, testFs, label='TestEs F1, max: '+str(max(testFs)))
+#semilogx
+plt.scatter(range(testNum), testFs)
+"""
+plt.plot(params, trainFs, label='TrainEs F1, max: '+str(max(trainFs)))
+plt.plot(params, testFs, label='TestEs F1, max: '+str(max(testFs)))
 #plt.plot(params, cvs, label='TestEs F1, max: '+str(max(cvs)))
-plt.xlabel('eps')
+plt.xlabel('selectedFeaureNum')
 plt.legend().draggable()
 plt.title(des)
+"""
 plt.show()
